4006ebbd4c1c32827298bf8d335c180c10c9f1f3
Mamta Nishad <mamta05082021@gmail.com>
2025-07-12 21:00:11 +0530

    Initial commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
-  const token = req.headers['authorization'];
+  const token = req.headers('authorization');
   if (!token) return res.status(403).send('No token');
   try {
     const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
